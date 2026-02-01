08c5ddfdfc8b5106814929f4cf2f47492aa93e93
shindesuraj21 <shinde.suraj21@gmail.com>
2026-02-01 16:31:25 -0700

    Update config.js 2

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me? (Turn on Play Music)",                                    // First interaction
+            text: "Do you like me? (Click on Play Music)",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
